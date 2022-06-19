a2f2b763e270645b7c6339008fdac02a4fea3189
Chang Shin <cshin7374@gmail.com>
2022-06-19 03:19:43 -0500

    commit tree f7c7fa1bd4941104672ef88582b4b421cd870d8d

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '' )
+  }
+  static titleize(string) {
+    const exclude = ['a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from', 'the' ]
+    const newArry =[]
+    const word = string.split(' ')
+    for(let i=0 ; i < word.length; i++) {
+      if(i===0) {
+        newArry.push(this.capitalize(word[0]))
+      } else {
+        if(exclude.includes(word[i])) {
+          newArry.push(word[i])
+        } else {
+          newArry.push(this.capitalize(word[i]))
+        }
+      }
+    }
+    return newArry.join(' ')
+  }
 }
